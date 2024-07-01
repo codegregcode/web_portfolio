5cc1e82e65cb12afecbd3623923a9a1d2d7e0f08
Greg Cain <codegregcode@gmail.com>
2024-07-01 14:04:30 +0100

    creates data file to load in projects

---
+++ src/data/projectsData.js
@@ -0,0 +1,28 @@
+const projectsData = [
+  {
+    name: 'photo_portfolio',
+    img_src: 'https://i.ibb.co/tZYpPc0/Screenshot-2024-06-30-at-21-22-06.png',
+    description:
+      'An online home for all my photos. Built using React, Firebase, react-spring and lightroomjs',
+    github_link: 'https://github.com/codegregcode/photo_portfolio',
+    deploy_link: 'https://github.com/codegregcode/photo_portfolio',
+  },
+  {
+    name: 'Pawfect Match',
+    img_src: 'https://i.ibb.co/tP7sDYH/Screenshot-2024-06-30-at-21-23-41.png',
+    description:
+      'The final project created as part of a small group on Command Shift bootcamp. Built using React, Firebase and more',
+    github_link: 'https://github.com/codegregcode/pawfect-match',
+    deploy_link: 'https://pawfect-match-23.netlify.app/',
+  },
+  {
+    name: 'NASA Image Seach - Command Shift Tech Test',
+    img_src: 'https://i.ibb.co/xMS6KFY/1681385310116.png',
+    description:
+      'Created to get a better understanding of a technical test as part of a tech interview. Built using React',
+    github_link: 'https://github.com/codegregcode/react-tech-test',
+    deploy_link: 'https://nasa-image-search-cgc23.netlify.app/',
+  },
+];
+
+export default projectsData;
